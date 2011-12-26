55cf4d2e9e66da922103591e0b8bc0d86f99bdbb
Dmitry Petrov <dpetroff@gmail.com>
2011-12-26 04:07:04 +0400

    implement subjects feature

--- client.js
+++ client.js
@@ -3,6 +3,8 @@ var client = {
     init: function() {
         this._pageContainer = document.querySelector('.b-singlepost');
 
+        if (!this._pageContainer) { return; } //we need only s1 pages with new design
+
         chrome.extension.sendRequest({action: 'getOptions'}, function(response) {
             _settings.setData(response);
 
@@ -19,6 +21,28 @@ var client = {
         if (value === 'smaller' || value === 'small') {
             this._pageContainer.classList.add('lje-commentsize-' + value);
         }
+    },
+
+    initShowsubjects: function(value, options) {
+        if (!value) { return; }
+
+        var form =  document.getElementById('postform'),
+            subject = form.subject,
+            subjectval = subject.value;
+
+        subject.type = 'text';
+        subject.size = 80;
+
+        var markup = '<div class="lje_subject"><label for="lje_subject">' + chrome.i18n.getMessage('client_subject') + 
+                    '</label></div>'
+            div = document.createElement('div');
+
+        div.innerHTML = markup;
+        var insertEl = div.firstChild,
+            comment = form.querySelector('.b-watering-comment');
+
+        comment.parentNode.insertBefore(insertEl, comment);
+        insertEl.appendChild(subject);
     }
 }
 
